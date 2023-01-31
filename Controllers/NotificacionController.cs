966412f51c6e75c55f29c1c46a00a0ccacad03dc
ronal <ronal@HP-GAMER>
2023-01-31 18:08:50 -0500

    subiendo

--- Controllers/NotificacionController.cs
+++ Controllers/NotificacionController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
 using SIGED_API.Contexts;
 using SIGED_API.Entity;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using MailKit.Net.Smtp;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -67,12 +71,31 @@ namespace SIGED_API.Controllers
         }
 
 
+        //[HttpPost("EnviarNotificacion/{id}")]
+        //public string Notificar([FromBody] NOTIFICACION notificacion)
+        //{
+
+        //    return "value";
+        //}
+
         [HttpPost("EnviarNotificacion/{id}")]
         public string Notificar([FromBody] NOTIFICACION notificacion)
         {
+            var email = new MimeMessage();
 
-            return "value";
+                email.From.Add(MailboxAddress.Parse("ronald.livia@outlook.com"));
+            email.To.Add(MailboxAddress.Parse("miguelkillki@gmail.com"));
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(TextFormat.Html) { Text = "<h1>HOLA</h1>" };
+        
+            using  var smtp = new MailKit.Net.Smtp.SmtpClient();
+            smtp.Connect("smtp.office365.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate("ronald.livia@outlook.com", "Yama314162$");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+            return  "OK";
         }
+
 
         // PUT api/<NotificacionController>/5
         //[HttpPut("{id}")]
